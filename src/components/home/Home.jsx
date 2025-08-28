7d71ef9c4b165fc447be3f6575f5c8dca287a193
Anirban986 <chakrabortyanirban228@gmail.com>
2025-08-28 09:49:37 +0530

    home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,59 +10,59 @@ import safty from '../../assets/safty.svg'
 
 import { Link } from 'react-router'
 function Home() {
-  return (
-    <div className='home'>
-    <div className="hero">
-        <h1>Empowering Schools and Colleges with <span>Disaster Preparedness</span></h1>
-        <p>Interactive learning platform for Indian schools and colleges to build disaster resilience through education, virtual drills, and gamified safety training.</p>
-    </div>
-    <div className="section1">
-        <div className="section-items">
-            <img src={school} alt="" />
-            <h1>1234+</h1>
-            <p>Schools enrolled</p>
-        </div>
-         <div className="section-items">
-            <img src={student} alt="" />
-            <h1>1234+</h1>
-            <p>Students trained</p>
-        </div>
-         <div className="section-items">
-            <img src={trophy} alt="" />
-            <h1>1234+</h1>
-            <p>Drills completed</p>
-        </div>
-         <div className="section-items">
-            <img src={achievement} alt="" />
-            <h1>60%</h1>
-            <p>Safty Score</p>
-        </div>
-    </div>
-    
-    <div className="section2">
-        <Link className='sec-link' to='/Learn' ><div className="button">Starting learning</div></Link>
-         <Link className='sec-link' to='/Vertual' ><div className="button-white">Try Vertual Drills</div></Link>
-    </div>
+    return (
+        <div className='home'>
+            <div className="hero">
+                <h1>Empowering Schools and Colleges with <span>Disaster Preparedness</span></h1>
+                <p>Interactive learning platform for Indian schools and colleges to build disaster resilience through education, virtual drills, and gamified safety training.</p>
+            </div>
+            <div className="section1">
+                <div className="section-items">
+                    <img src={school} alt="" />
+                    <h1>1234+</h1>
+                    <p>Schools enrolled</p>
+                </div>
+                <div className="section-items">
+                    <img src={student} alt="" />
+                    <h1>1234+</h1>
+                    <p>Students trained</p>
+                </div>
+                <div className="section-items">
+                    <img src={trophy} alt="" />
+                    <h1>1234+</h1>
+                    <p>Drills completed</p>
+                </div>
+                <div className="section-items">
+                    <img src={achievement} alt="" />
+                    <h1>60%</h1>
+                    <p>Safty Score</p>
+                </div>
+            </div>
 
-    <div className="section3">
-        <div className="section3-items">
-            <img src={map} alt="" />
-            <h1>Interactive Learning</h1>
-            <p>Region-specific disaster education modules</p>
-        </div> 
-         <div className="section3-items">
-            <img src={activity} alt="" />
-            <h1>Virtual Drills</h1>
-            <p>Practice emergency procedures safely</p>
+            <div className="section2">
+                <Link className='sec-link' to='/Learn' ><div className="button">Starting learning</div></Link>
+                <Link className='sec-link' to='/Vertual' ><div className="button-white">Try Vertual Drills</div></Link>
+            </div>
+
+            <div className="section3">
+                <div className="section3-items">
+                    <img src={map} alt="" />
+                    <h1>Interactive Learning</h1>
+                    <p>Region-specific disaster education modules</p>
+                </div>
+                <div className="section3-items">
+                    <img src={activity} alt="" />
+                    <h1>Virtual Drills</h1>
+                    <p>Practice emergency procedures safely</p>
+                </div>
+                <div className="section3-items">
+                    <img src={safty} alt="" />
+                    <h1>Safety Games</h1>
+                    <p>Learn through engaging gameplay</p>
+                </div>
+            </div>
         </div>
-         <div className="section3-items">
-            <img src={safty} alt="" />
-            <h1>Safety Games</h1>
-            <p>Learn through engaging gameplay</p>
-        </div>
-    </div>
-    </div>
-  )
+    )
 }
 
 export default Home
